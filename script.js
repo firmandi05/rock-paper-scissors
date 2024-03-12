04b739a0c4dd48498ff22ef8ee3b33b70ffb8606
Hairu Firmandi <firmandi05@gmail.com>
2024-03-12 22:33:22 +0700

    with ui and added some logic change

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const capitalizeFirstLetter = (string) =>
-  string.charAt(0).toUpperCase() + string.slice(1);
+// const capitalizeFirstLetter = (string) =>
+//   string.charAt(0).toUpperCase() + string.slice(1);
 let playerSelection = getPlayerChoice();
 let computerSelection = getComputerChoice();
+let CreateResultControl = true;
+let createResultAnnouncementControl = true;
+
+let playerScoreNode = document.querySelector("div.score p.player");
+let compScoreNode = document.querySelector("div.score p.computer");
+playerScoreNode.textContent = "";
+compScoreNode.textContent = "";
 
 function getComputerChoice() {
   const pick = ["Rock", "Paper", "Scissors"];
@@ -12,41 +19,172 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-  let playerPick = prompt(`Choose between Rock, Paper, Or Scissors :`, "");
+  let rock = document.querySelector("button.rock");
+  rock.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+  });
 
-  // check if prompt is close or empty
-  while (playerPick === null || playerPick.trim() === "") {
-    alert("Please pick Rock, Paper, or Scissors");
+  let paper = document.querySelector("button.paper");
+  paper.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+  });
 
-    return getPlayerChoice();
+  let scissors = document.querySelector("button.scissors");
+  scissors.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+  });
+}
+
+function CreateResultDiv() {
+  const resultContainer = document.createElement("div");
+  resultContainer.classList.add("vs");
+  document.body.appendChild(resultContainer);
+
+  const playerContainer = document.createElement("div");
+  playerContainer.classList.add("player");
+  resultContainer.appendChild(playerContainer);
+
+  const playerName = document.createElement("p");
+  playerName.textContent = "You";
+  playerContainer.appendChild(playerName);
+
+  const playerPick = document.createElement("p");
+  playerPick.classList.add("player");
+  playerContainer.appendChild(playerPick);
+
+  const playerIcon = document.createElement("i");
+  playerIcon.classList.add("player");
+  playerPick.appendChild(playerIcon);
+
+  const h2 = document.createElement("h2");
+  h2.textContent = "VS";
+  resultContainer.appendChild(h2);
+
+  const compContainer = document.createElement("div");
+  compContainer.classList.add("comp");
+  resultContainer.appendChild(compContainer);
+
+  const compName = document.createElement("p");
+  compName.textContent = "Comp";
+  compContainer.appendChild(compName);
+
+  const compPick = document.createElement("p");
+  compPick.classList.add("comp");
+  compContainer.appendChild(compPick);
+
+  const compIcon = document.createElement("i");
+  compIcon.classList.add("comp");
+  compPick.appendChild(compIcon);
+}
+
+function createIconPick() {
+  const createElement = document.createElement("i");
+  createElement.classList.add("");
+}
+
+function createResultAnnouncement() {
+  const createElement = document.createElement("h1");
+  createElement.classList.add("result");
+  document.body.appendChild(createElement);
+}
+
+function addScore(node) {
+  let currentValue = parseInt(node.textContent.trim() || 0, 10);
+  node.textContent = currentValue + 1;
+}
+
+function determineWinner() {
+
+  const resultAnnouncement = document.querySelector("h1.result");
+
+  const playerValue = parseInt(playerScoreNode.textContent);
+  const compValue = parseInt(compScoreNode.textContent)
+
+  if (playerValue === 5 ) {
+    resultAnnouncement.textContent = 'Player WIN!!'
+
+    document.body.addEventListener('click', function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      return false;
+  }, true);
+  
   }
 
-  // Make playerPick lowercase
-  let lowerCasePlayerPick = playerPick.toLowerCase();
+  if (compValue === 5 ) {
+    resultAnnouncement.textContent = 'Computer WIN!!'
 
-  // check if prompt is not Rock, Paper, or Scissors
-  const validChoice = ["rock", "paper", "scissors"];
-  while (!validChoice.includes(lowerCasePlayerPick)) {
-    alert("Please enter Rock, Paper, or Scissors");
-
-    return getPlayerChoice();
+    document.body.addEventListener('click', function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      return false;
+  }, true);
+  
   }
-
-  return playerPick.toLowerCase();
 }
 
 function playRound(player, computer) {
-  if (player === computer) {
-    return `You chose ${capitalizeFirstLetter(player)}. Computer chose ${capitalizeFirstLetter(computer)}. It's a tie!`;
-  } else if (player === "rock" && computer === "scissors") {
-    return `You chose ${capitalizeFirstLetter(player)}. Computer chose ${capitalizeFirstLetter(computer)}. Oh my god!! You win!`;
-  }else if (player === "paper" && computer === "rock") {
-    return `You chose ${capitalizeFirstLetter(player)}. Computer chose ${capitalizeFirstLetter(computer)}. Oh my god!! You win!`;
-  }else if (player === "scissors" && computer === "paper") {
-    return `You chose ${capitalizeFirstLetter(player)}. Computer chose ${capitalizeFirstLetter(computer)}. Oh my god!! You win!`;
-  } else {
-    return `You chose ${capitalizeFirstLetter(player)}. Computer chose ${capitalizeFirstLetter(computer)}. Oh no!, You lose!`
+  if (CreateResultControl) {
+    CreateResultDiv();
+    CreateResultControl = false;
   }
-}
+  if (createResultAnnouncementControl) {
+    createResultAnnouncement();
+    createResultAnnouncementControl = false;
+  }
 
-console.log(playRound(playerSelection, computerSelection));
+  const playerPick = document.querySelector("i.player");
+  const compPick = document.querySelector("i.comp");
+  const resultAnnouncement = document.querySelector("h1.result");
+
+  playerPick.className = "player";
+  compPick.className = "comp";
+
+  const pick = {
+    rock: (user) => {
+      user.classList.add("fa-solid", "fa-hand-back-fist", "fa-2xl");
+    },
+    paper: (user) => {
+      user.classList.add("fa-solid", "fa-hand", "fa-2xl");
+    },
+    scissors: (user) => {
+      user.classList.add(
+        "fa-solid",
+        "fa-hand-scissors",
+        "fa-rotate-90",
+        "fa-2xl"
+      );
+    },
+  };
+
+  if (player === computer) {
+    pick[player](playerPick);
+    pick[computer](compPick);
+    resultAnnouncement.textContent = `It's a Tie!`;
+  } else if (player === "rock" && computer === "scissors") {
+    addScore(playerScoreNode);
+    pick[player](playerPick);
+    pick[computer](compPick);
+    resultAnnouncement.textContent = `You Win!`;
+  } else if (player === "paper" && computer === "rock") {
+    addScore(playerScoreNode);
+    pick[player](playerPick);
+    pick[computer](compPick);
+    resultAnnouncement.textContent = `You Win!`;
+  } else if (player === "scissors" && computer === "paper") {
+    addScore(playerScoreNode);
+
+    pick[player](playerPick);
+    pick[computer](compPick);
+    resultAnnouncement.textContent = `You Win!`;
+  } else {
+    addScore(compScoreNode);
+
+    pick[player](playerPick);
+    pick[computer](compPick);
+    resultAnnouncement.textContent = `Comp Win!`;
+  }
+
+  determineWinner();
+
+}
